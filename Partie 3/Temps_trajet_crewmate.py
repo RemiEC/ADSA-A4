f7479cc766eb08e6c109649d2be8e457c533d3e1
Rémi GUILLON <guillonbonyremi@gmail.com>
2020-12-13 19:08:40 +0100

    Fini code partie 3 mais pour le simposteurs ça marche pas

---
+++ Partie 3/Temps_trajet_crewmate.py
@@ -0,0 +1,120 @@
+import math
+import pandas as pd
+'''
+#! Order of the rooms for rows and columns
+Upper E
+Lower E
+Reactor
+Security
+Cafeteria
+Medbay
+Weapons
+O2
+Navigation
+Shield
+Storage
+Electrical
+Between Cafet-Storage
+Between Storage-Shield
+Vent (only for Impostors)
+
+'''
+inf = math.inf
+# We set the value infinite to represent an impossible movement between 2 rooms
+adjacency_matrix_crewmate = [
+    [0, 7, 5.5, 5.5, 9, 7,inf,inf,inf,inf,inf,inf,inf,inf],
+    [7,0, 5.5, 5.5,inf,inf,inf,inf,inf,inf, 10, 8.5,inf,inf],
+    [5.5, 5.5,0, 4,inf,inf,inf,inf,inf,inf,inf,inf,inf,inf],
+    [5.5, 5.5, 4,0,inf,inf,inf,inf,inf,inf,inf,inf,inf,inf],
+    [9,inf,inf,inf,0, 7, 5.5,inf,inf,inf, 8.5,inf, 7.5,inf],
+    [7,inf,inf,inf, 7,0,inf,inf,inf,inf,inf,inf,inf,inf],
+    [inf,inf,inf,inf, 5.5,inf,0, 4.5, 7.5, 10,inf,inf,inf,inf],
+    [inf,inf,inf,inf,inf,inf, 4.5,0, 7, 9.5,inf,inf,inf,inf],
+    [inf,inf,inf,inf,inf,inf, 7.5, 7,0, 8.5,inf,inf,inf,inf],
+    [inf,inf,inf,inf,inf,inf, 10, 9.5, 8.5,0, 6,inf,inf, 4],
+    [inf, 10,inf,inf, 8.5,inf,inf,inf,inf, 6,0, 7.5, 7, 6],
+    [inf, 8.5,inf,inf,inf,inf,inf,inf,inf,inf, 7.5,0,inf,inf],
+    [inf,inf,inf,inf, 7.5,inf,inf,inf,inf,inf, 7,inf,0,inf],
+    [inf,inf,inf,inf,inf,inf,inf,inf,inf, 4, 6,inf,inf,0]
+]
+
+Room_list_crewmate = ["Upper E",
+"Lower E",
+"Reactor",
+"Security",
+"Cafeteria",
+"Medbay",
+"Weapons",
+"O2",
+"Navigation",
+"Shield",
+"Storage",
+"Electrical",
+"Between Cafet-Storage",
+"Between Storage-Shield"]
+
+
+# We created this adjacency matrix by taking the shortest path available between 2 adjacent rooms when 2 options were available : regular path and vent
+adjacency_matrix_impostor = [
+    [0, 7, inf, 5.5, 9, 7, inf, inf, inf, inf, inf, inf, inf, inf, inf],
+    [7,0, inf, 5.5, inf, inf, inf, inf, inf,inf, 10, 8.5,inf,inf,inf],
+    [inf,inf,0, 4,inf,inf,inf,inf,inf,inf,inf,inf,inf,inf,inf],
+    [5.5, 5.5, 4,0,inf,inf,inf,inf,inf,inf,inf,inf,inf,inf,inf],
+    [9,inf,inf,inf,0, 7, 5.5,inf,inf,inf, 8.5,inf,inf,inf,inf],
+    [7,inf,inf,inf, 7,0,inf,inf,inf,inf,inf,inf,inf,inf,inf],
+    [inf,inf,inf,inf, 5.5,inf,0, 4.5,inf, 10,inf,inf,inf,inf, 6.5],
+    [inf,inf,inf,inf,inf,inf, 4.5,0, 7, 9.5,inf,inf,inf,inf, 6],
+    [inf,inf,inf,inf,inf,inf,inf, 7,0,inf,inf,inf,inf,inf, 5],
+    [inf,inf,inf,inf,inf,inf, 10, 9.5,inf,0, 6,inf,inf, 4, 3.5],
+    [inf, 10,inf,inf, 8.5,inf,inf,inf,inf, 6,0, 7.5, 7, 6,inf],
+    [inf, 8.5,inf,inf,inf,inf,inf,inf,inf,inf, 7.5,0,inf,inf,inf],
+    [inf,inf,inf,inf,inf,inf,inf,inf,inf,inf, 7,inf,0,inf,inf],
+    [inf,inf,inf,inf,inf,inf,inf,inf,inf, 4, 6,inf,inf,0,inf],
+    [inf,inf,inf,inf,inf,inf, 6.5, 6, 5, 3.5,inf,inf,inf,inf,0],
+]
+
+Room_list_impostor = ["Upper E",
+"Lower E",
+"Reactor",
+"Security",
+"Cafeteria",
+"Medbay",
+"Weapons",
+"O2",
+"Navigation",
+"Shield",
+"Storage",
+"Electrical",
+"Between Cafet-Storage",
+"Between Storage-Shield",
+"Vent btwn Shield-Nav"]
+
+'''
+#* Function printing a dataframe containing the travel time between each pair of room for crewmate or impostor
+
+#? adjacency_matrix : Adjacency matrix of impostor or crewmate created from the png graph in the report
+#? Room_list : List of the Rooms
+'''
+def Floyd_Warshall_Algorithm(adjacency_matrix, Room_list):
+    taille_matrix = len(adjacency_matrix)
+    for k in range (taille_matrix):
+        for i in range (taille_matrix):
+            for j in range(taille_matrix):
+                if(adjacency_matrix[i][j] > adjacency_matrix[i][k] + adjacency_matrix[k][j]):
+                    adjacency_matrix[i][j] = adjacency_matrix[i][k] + adjacency_matrix[k][j]
+
+    #Now we display the result
+    dataframe = pd.DataFrame(adjacency_matrix)
+    dataframe.columns = Room_list
+    dataframe.index = Room_list
+    pd.set_option("display.max_rows", None, "display.max_columns", None, 'display.width', None)
+    print(dataframe)
+        
+    
+
+
+# CREWMATE
+# Floyd_Warshall_Algorithm(adjacency_matrix_crewmate, Room_list_crewmate)
+
+# IMPOSTOR
+Floyd_Warshall_Algorithm(adjacency_matrix_impostor, Room_list_impostor)
